053120e0a5552cd2f2550a506a22e3831446cbe1
Nikonov <vipergordon@outlook.com>
2018-12-26 07:54:25 +0200

    add HelloProvider and HiProvider

---
+++ TestTaskNikonov/Providers/HelloProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestTaskNikonov.Interfaces;
+
+namespace TestTaskNikonov.Providers
+{
+    public class HelloProvider : IGreeter
+    {
+        public string SayHello()
+        {
+            return "Hi everyone!";
+        }
+    }
+}
---
+++ TestTaskNikonov/Providers/HiProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestTaskNikonov.Interfaces;
+
+namespace TestTaskNikonov.Providers
+{
+    public class HiProvider : IGreeter
+    {
+        public string SayHello()
+        {
+            return "Hi there!";
+        }
+    }
+}
